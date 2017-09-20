dbc109cf3528b66da55a99a9da73029dcf92d5ee
gillybops <gillybops@optonline.net>
2017-09-20 01:13:44 +0000

    Third Field Added

--- db/schema.rb
+++ db/schema.rb
@@ -17,9 +17,10 @@ ActiveRecord::Schema.define(version: 20170919233903) do
 
   create_table "quotes", force: :cascade do |t|
     t.string   "saying"
+    t.string   "translation"
     t.string   "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
